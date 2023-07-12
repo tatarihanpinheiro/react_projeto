4f79a027630ecdde0189b5e762d922c30d94a295
tatianarihanpinheiro <tata_rihan@hotmail.com>
2023-07-12 20:00:15 -0300

    adicionar motivo de viagens

---
+++ motivodeviagens/README.md
@@ -0,0 +1,52 @@
+<h1 align="center">Tipo de Viagem Por Período</h1>
+
+<p align="center">
+  <strong>Por Abner Santos</strong>
+</p>
+
+<p align="center">
+  <strong>A partir de uma data selecionada, descubra se as pessoas viajam mais a trabalho ou a lazer durante o período escolhido. Isso é feito com base em um código IATA de aeroporto de partida e destino, juntamente com as datas de partida e retorno.</strong>
+</p>
+
+## Descrição
+
+O programa utiliza a API da Amadeus para realizar previsões sobre o propósito da viagem, considerando o período especificado pelo usuário. Ao informar os códigos IATA dos aeroportos de partida e destino, bem como as datas de partida e retorno, o programa faz uma consulta à API e retorna o tipo de viagem predominante para esse período.
+
+A análise é baseada em dados históricos e tendências de viagem, permitindo uma estimativa de se a maioria das viagens nesse período é para trabalho ou lazer. O resultado é exibido em uma interface amigável ao usuário, que mostra o tipo de viagem predominante para as datas e locais especificados.
+
+## Recursos
+
+- Digite os códigos IATA dos aeroportos de partida e destino.
+- Selecione as datas de partida e retorno.
+- Obtenha a previsão do tipo de viagem predominante para o período especificado.
+
+## Capturas de Tela
+
+<section align="center">
+  
+![image](https://github.com/AbnerSantosss/motivoDeViajens/assets/107922389/b500c427-78c7-4643-a158-67cfd23a62a0)
+
+![Alt text](image.png)
+
+</section>
+
+## Como usar
+
+1. Abra o arquivo `index.html` em um navegador web.
+2. Preencha os campos com os códigos IATA dos aeroportos de partida e destino.
+3. Selecione as datas de partida e retorno.
+4. Clique no botão "Buscar".
+5. O programa consultará a API e exibirá o tipo de viagem predominante para o período especificado.
+
+## Nota
+
+Este programa utiliza a API da Amadeus. É necessário ter uma chave de API válida para acessar os serviços da Amadeus. Certifique-se de substituir a chave de API no arquivo `script.js` antes de usar o programa.
+
+## Tecnologias utilizadas
+
+- HTML
+- CSS
+- JavaScript
+- API da Amadeus
+
+
---
+++ motivodeviagens/script.js
@@ -0,0 +1,95 @@
+// Obtém a referência do botão que abre o modal
+const openModalBtn = document.getElementById('open-modal-btn');
+// Obtém a referência do elemento do modal
+const modal = document.getElementById('modal');
+let showDisplayTT = document.querySelector('.showDisplay');
+
+// Define um evento para fechar o modal ao clicar no botão de fechar
+const closeModalBtn = document.getElementById('close-modal-btn');
+closeModalBtn.addEventListener('click', function() {
+  modal.style.display = 'none';
+});
+
+// Define um evento para fechar o modal ao clicar fora dele
+window.addEventListener('click', function(event) {
+  if (event.target === modal) {
+    modal.style.display = 'none';
+  }
+});
+
+
+// Função para obter o token
+function getToken() {
+  const tokenUrl = 'https://test.api.amadeus.com/v1/security/oauth2/token';
+  const clientId = '3Pp4q6AkjSbxDC2QUlwrACi6LAdLhbGO';
+  const clientSecret = 'SGjlYFHaPXCDn8Id';
+
+  // Cria um objeto URLSearchParams para enviar os parâmetros na requisição
+  const params = new URLSearchParams();
+  params.append('grant_type', 'client_credentials');
+  params.append('client_id', clientId);
+  params.append('client_secret', clientSecret);
+
+  // Retorna uma Promise que faz a requisição para obter o token
+  return fetch(tokenUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded'
+      },
+      body: params
+    })
+    .then(response => response.json())
+    .then(data => data.access_token) // Retorna o token obtido da resposta da requisição
+    .catch(error => {
+      console.log(error);
+      throw error;
+    });
+}
+getToken()
+
+// Função para obter o resultado
+function getResult() {
+  const origin = document.getElementById('origin').value;
+  const destination = document.getElementById('destination').value;
+  const departureDate = document.getElementById('departure-date').value;
+  const returnDate = document.getElementById('return-date').value;
+
+  if (origin === '' || destination === '' || departureDate === '' || returnDate === '') {
+    return alert('Preencha todos os campos!');
+  }
+
+  // Chama a função getToken() para obter o token necessário para a requisição
+  getToken()
+    .then(token => {
+      const url = `https://test.api.amadeus.com/v1/travel/predictions/trip-purpose?originLocationCode=${origin}&destinationLocationCode=${destination}&departureDate=${departureDate}&returnDate=${returnDate}`;
+      
+      // Faz a requisição para a API Amadeus usando o token obtido
+      fetch(url, {
+        method: 'GET',
+        headers: {
+          'Authorization': `Bearer ${token}` // Usa o token no cabeçalho da requisição
+        },
+      })
+        .then(response => response.json())
+        .then(data => {
+          const { result } = data.data;
+          console.log(result);
+          modal.style.display = 'block';
+
+          showDisplayTT.innerHTML = `
+            <h3>Geralmente nesse período as pessoas viajam a ${result}</h3>
+          `;
+        })
+        .catch(error => {
+          // Em caso de erro na requisição, exibe mensagem de erro
+          showDisplayTT.innerHTML = `
+            <h3>Erro ${error}</h3>
+          `;
+          console.log(error);
+        });
+    })
+    .catch(error => {
+      // Em caso de erro ao obter o token, exibe mensagem de erro
+      console.log(error);
+    });
+}
